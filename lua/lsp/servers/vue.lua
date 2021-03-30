f1976bb419bee8ed9dbd4f7923b9ff79e9133708
lbavendanos <lbavendanos@gmail.com>
2021-03-30 12:35:54 -0500

    Fix vue lsp

--- lua/lsp/servers/vue.lua
+++ lua/lsp/servers/vue.lua
@@ -1,4 +1,7 @@
 local lspconfig = require('lspconfig')
 local on_attach = require('partials.lspconfig').on_attach
 
-lspconfig.vuels.setup { on_attach = on_attach }
+lspconfig.vuels.setup {
+  on_attach = on_attach,
+  init_options = { config = { vetur = { completion = { autoImport = true } } } }
+}
