ce770fae210d213de575dd5b56c7dc7d80043056
lbavendanos <lbavendanos@gmail.com>
2021-03-30 00:35:21 -0500

    Fix indentline

--- lua/partials/indentline/init.lua
+++ lua/partials/indentline/init.lua
@@ -1,4 +1,4 @@
-vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
+vim.g.indent_blankline_filetype_exclude = { 'help', 'packer', 'lspinfo' }
 vim.g.indent_blankline_char = '▏'
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 
