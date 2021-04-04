4fb931c5ed051dbb1109193eb46bd68524f09328
lbavendanos <lbavendanos@gmail.com>
2021-04-03 20:54:54 -0500

    Add emmet

---
+++ lua/lsp/servers/emmet.lua
@@ -0,0 +1,20 @@
+local lspconfig = require('lspconfig')
+local configs = require('lspconfig/configs')
+local on_attach = require('partials.lspconfig').on_attach
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+configs.emmet_ls = {
+  default_config = {
+    cmd = { 'emmet-ls', '--stdio' },
+    filetypes = {
+      'html', 'css', 'typescriptreact', 'typescript.tsx', 'javascriptreact',
+      'javascript.jsx', 'vue'
+    },
+    root_dir = function() return vim.loop.cwd() end,
+    settings = {}
+  }
+}
+
+lspconfig.emmet_ls.setup { on_attach = on_attach }
--- lua/lsp/servers/html.lua
+++ lua/lsp/servers/html.lua
@@ -1,7 +1,7 @@
 local lspconfig = require('lspconfig')
 local on_attach = require('partials.lspconfig').on_attach
-
 local capabilities = vim.lsp.protocol.make_client_capabilities()
+
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 lspconfig.html.setup { on_attach = on_attach, capabilities = capabilities }
