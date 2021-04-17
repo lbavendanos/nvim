830856c8b78ea86aa23d56cfc4b5f3bc92d9cc15
lbavendanos <lbavendanos@gmail.com>
2021-04-17 16:25:44 -0500

    Fix tree

--- lua/partials/tree/init.lua
+++ lua/partials/tree/init.lua
@@ -9,19 +9,19 @@ vim.g.nvim_tree_show_icons = { git = 0, folders = 1, files = 1 }
 
 -- vim.cmd('augroup Cursor')
 -- vim.cmd('autocmd!')
--- vim.cmd('autocmd WinLeave,FileType NvimTree setlocal guicursor=n-v-c-sm:block,i-ci-ve:ver2u,r-cr-o:hor20,')
--- vim.cmd('autocmd WinEnter,FileType NvimTree setlocal guicursor=n-c-v:block-Cursor/Cursor-blinkon0,')
--- vim.cmd('autocmd WinLeave,FileType NvimTree setlocal nocursorline')
--- vim.cmd('autocmd WinEnter,FileType NvimTree setlocal cursorline')
+-- vim.cmd('autocmd BufLeave,FileType NvimTree setlocal guicursor=n-v-c-sm:block,i-ci-ve:ver2u,r-cr-o:hor20,')
+-- vim.cmd('autocmd BufEnter,FileType NvimTree setlocal guicursor=n-c-v:block-Cursor/Cursor-blinkon0,')
+-- vim.cmd('autocmd BufLeave,FileType NvimTree setlocal nocursorline')
+-- vim.cmd('autocmd BufEnter,FileType NvimTree setlocal cursorline')
 -- vim.cmd('augroup END')
 -- vim.cmd('autocmd FileType NvimTree hi Cursor blend=100')
 
 vim.api.nvim_exec([[
 augroup Cursor
   au!
-  au WinLeave,FileType NvimTree setlocal guicursor=n-v-c-sm:block,i-ci-ve:ver2u,r-cr-o:hor20,
-  au WinEnter,FileType NvimTree setlocal guicursor=n-c-v:block-Cursor/Cursor-blinkon0,
-  au WinEnter,FileType NvimTree setlocal cursorline
+  au BufLeave,FileType NvimTree setlocal guicursor=n-v-c-sm:block,i-ci-ve:ver2u,r-cr-o:hor20,
+  au BufEnter,FileType NvimTree setlocal guicursor=n-c-v:block-Cursor/Cursor-blinkon0,
+  au BufEnter,FileType NvimTree setlocal cursorline
 augroup END
 au FileType NvimTree hi Cursor blend=100
 ]], false)
