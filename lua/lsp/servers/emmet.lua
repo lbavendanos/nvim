b5b28fba5bd28a8fd246d1cb5f74ef8861beb2a7
lbavendanos <lbavendanos@gmail.com>
2021-04-20 11:55:14 -0500

    Fix emmet

--- lua/lsp/servers/emmet.lua
+++ lua/lsp/servers/emmet.lua
@@ -8,10 +8,7 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 configs.emmet_ls = {
   default_config = {
     cmd = { 'emmet-ls', '--stdio' },
-    filetypes = {
-      'html', 'css', 'typescriptreact', 'typescript.tsx', 'javascriptreact',
-      'javascript.jsx', 'vue'
-    },
+    filetypes = { 'html', 'css' },
     root_dir = function() return vim.loop.cwd() end,
     settings = {}
   }
