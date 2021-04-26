b80f4a08c49350693dac1d080f2cafc97bc0a3fa
lbavendanos <lbavendanos@gmail.com>
2021-04-26 12:11:35 -0500

    Add cursorline

--- lua/partials/tree/init.lua
+++ lua/partials/tree/init.lua
@@ -7,7 +7,7 @@ vim.g.nvim_tree_disable_netrw = 0
 vim.g.nvim_tree_hijack_netrw = 0
 vim.g.nvim_tree_show_icons = { git = 0, folders = 1, files = 1 }
 
-vim.cmd('autocmd BufWinEnter,FileType NvimTree setlocal cursorline')
+-- vim.cmd('autocmd BufWinEnter,FileType NvimTree setlocal cursorline')
 
 vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>',
                         { noremap = true })
