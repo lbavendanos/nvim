3f82c87f0ae3f5f8158e07a743efd9b25e402b56
lbavendanos <lbavendanos@gmail.com>
2021-06-02 17:27:10 -0500

    Fix tree

--- lua/partials/tree/init.lua
+++ lua/partials/tree/init.lua
@@ -5,7 +5,12 @@ vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_disable_netrw = 0
 vim.g.nvim_tree_hijack_netrw = 0
-vim.g.nvim_tree_show_icons = { git = 0, folders = 1, files = 1 }
+vim.g.nvim_tree_show_icons = {
+  git = 0,
+  folders = 1,
+  files = 1,
+  folder_arrows = 0
+}
 
 -- vim.cmd('autocmd BufWinEnter,FileType NvimTree setlocal cursorline')
 
