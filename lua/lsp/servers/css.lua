6474828cd0a294d30e76c65ffe78a1b39296cf3d
lbavendanos <lbavendanos@gmail.com>
2021-06-15 10:45:26 -0500

    Fix css language

--- lua/lsp/servers/css.lua
+++ lua/lsp/servers/css.lua
@@ -1,4 +1,7 @@
 local lspconfig = require('lspconfig')
 local on_attach = require('partials.lspconfig').on_attach
+local capabilities = vim.lsp.protocol.make_client_capabilities()
 
-lspconfig.cssls.setup { on_attach = on_attach }
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+lspconfig.cssls.setup { on_attach = on_attach, capabilities = capabilities }
