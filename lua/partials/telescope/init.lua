f98efe17627a4ec76220e24961abeb8504d82d12
lbavendanos <lbavendanos@gmail.com>
2021-07-01 10:45:17 -0500

    Fix telescope

--- lua/partials/telescope/init.lua
+++ lua/partials/telescope/init.lua
@@ -24,8 +24,10 @@ require('telescope').setup {
     layout_strategy = 'center',
     results_title = false,
     preview_title = 'Preview',
-    preview_cutoff = 1, -- Preview should always show (unless previewer = false)
-    width = 80,
-    results_height = 15
+    layout_config = {
+      preview_cutoff = 1, -- Preview should always show (unless previewer = false)
+      width = 80,
+      height = 15
+    }
   }
 }
