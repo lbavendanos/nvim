cc692a7e13500eac733c3bafb8563905c36cd4e3
lbavendanos <lbavendanos@gmail.com>
2021-12-09 17:55:56 -0500

    Fix tsserver

--- lua/lsp/servers/tsserver.lua
+++ lua/lsp/servers/tsserver.lua
@@ -1,4 +1,11 @@
 local lspconfig = require('lspconfig')
 local on_attach = require('partials.lspconfig').on_attach
 
-lspconfig.tsserver.setup({ on_attach = on_attach })
+lspconfig.tsserver.setup({
+  on_attach = function(client, bufnr)
+    on_attach(client, bufnr)
+
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+  end,
+})
