ed9ad65272552b15b4286596d0a708c6c4ce58cd
lbavendanos <lbavendanos@gmail.com>
2021-12-12 15:32:44 -0500

    Fix main theme

--- lua/themes/init.lua
+++ lua/themes/init.lua
@@ -1,17 +1,14 @@
+local colors = require('utils.colors')
+
 require('themes.nightfly')
 -- require('themes.moonfly')
 
-local colors = { black = '#000000', gray = '#181A1F' }
-
 vim.cmd('highlight Normal guibg=' .. colors.black)
-vim.cmd('highlight VertSplit guibg=' .. colors.black .. ' guifg=' .. colors.gray)
+vim.cmd('highlight VertSplit guibg=' .. colors.black .. ' guifg=' .. colors.dark_blue)
 
-vim.api.nvim_exec(
-  [[
+vim.cmd([[
   set guicursor=n-v-c:block-Cursor
   set guicursor+=i:ver100-iCursor
   set guicursor+=n-v-c:blinkon0
   set guicursor+=i:blinkwait10
-]],
-  false
-)
+]])
