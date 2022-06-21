d9287675faf7603bc64d50cfc3a457ee90791e13
lbavendanos <lbavendanos@gmail.com>
2022-06-21 18:48:35 -0500

    Fix cmp

--- lua/partials/cmp/init.lua
+++ lua/partials/cmp/init.lua
@@ -1,6 +1,5 @@
 local cmp = require('cmp')
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local luasnip = require('luasnip')
 local lspkind = require('lspkind')
 
 local source_mapping = {
@@ -14,33 +13,6 @@ local source_mapping = {
   calc = '[Calc]',
   emoji = '[Emoji]',
 }
-
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-end
-
-local function tab(fallback)
-  if cmp.visible() then
-    cmp.select_next_item()
-  elseif luasnip.expand_or_jumpable() then
-    luasnip.expand_or_jump()
-  elseif has_words_before() then
-    cmp.complete()
-  else
-    fallback()
-  end
-end
-
-local function shift_tab(fallback)
-  if cmp.visible() then
-    cmp.select_prev_item()
-  elseif luasnip.jumpable(-1) then
-    luasnip.jump(-1)
-  else
-    fallback()
-  end
-end
 
 cmp.setup({
   snippet = {
@@ -59,8 +31,6 @@ cmp.setup({
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    ['<Tab>'] = cmp.mapping(tab, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(shift_tab, { 'i', 's' }),
   }),
   sources = cmp.config.sources({
     { name = 'buffer' },
