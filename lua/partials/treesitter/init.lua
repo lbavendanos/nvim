fc8ff02be7a5d6020578916938868b3b4ac9bc76
lbavendanos <lbavendanos@gmail.com>
2022-07-15 10:37:04 -0500

    Fix treesitter

--- lua/partials/treesitter/init.lua
+++ lua/partials/treesitter/init.lua
@@ -2,7 +2,7 @@ require('nvim-treesitter.configs').setup({
   ensure_installed = 'all', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true, -- false will disable the whole extension
-    -- disable = { 'php', 'vue', 'tsx', 'typescript' }, -- list of language that will be disabled
+    additional_vim_regex_highlighting = false
   },
   indent = { enable = true },
   autotag = { enable = true },
