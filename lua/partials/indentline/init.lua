525dafe1f2c886ba85d060f0ead4c2c2b5b11856
lbavendanos <lbavendanos@gmail.com>
2022-09-01 13:24:12 -0500

    Remomve deprecated partials

--- lua/partials/indentline/init.lua
+++
@@ -1,5 +0,0 @@
-vim.g.indent_blankline_filetype_exclude = { 'help', 'packer', 'lspinfo' }
-vim.g.indent_blankline_char = '▏'
-vim.g.indent_blankline_show_trailing_blankline_indent = false
-
-vim.cmd('highlight IndentBlanklineChar guifg=#121212 gui=nocombine')
