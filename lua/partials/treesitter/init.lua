99226d7350059d7fe48aa67656f4d34443c583fd
lbavendanos <lbavendanos@gmail.com>
2022-10-08 12:18:18 -0500

    Ignore phpdoc

--- lua/partials/treesitter/init.lua
+++ lua/partials/treesitter/init.lua
@@ -1,5 +1,6 @@
 require('nvim-treesitter.configs').setup({
   ensure_installed = 'all', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ignore_install = { 'phpdoc' },
   highlight = {
     enable = true, -- false will disable the whole extension
     additional_vim_regex_highlighting = false,
