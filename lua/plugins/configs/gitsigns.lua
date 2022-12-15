c61b9870a01169345d92487eaf97cd3320755d41
lbavendanos <lbavendanos@gmail.com>
2022-12-15 12:39:05 -0500

    Fix gitsigns

--- lua/plugins/configs/gitsigns.lua
+++ lua/plugins/configs/gitsigns.lua
@@ -46,12 +46,12 @@ gitsigns.setup({
     map('n', '<leader>hb', function()
       gs.blame_line({ full = true })
     end)
-    map('n', '<leader>tb', gs.toggle_current_line_blame)
+    -- map('n', '<leader>tb', gs.toggle_current_line_blame)
     map('n', '<leader>hd', gs.diffthis)
     map('n', '<leader>hD', function()
       gs.diffthis('~')
     end)
-    map('n', '<leader>td', gs.toggle_deleted)
+    -- map('n', '<leader>td', gs.toggle_deleted)
 
     -- Text object
     map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
