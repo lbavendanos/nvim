39999259988a317c6f44d026d98fff78f0c458cf
lbavendanos <lbavendanos@gmail.com>
2023-03-06 12:32:16 -0500

    Fix colorschema

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -6,17 +6,22 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      local dc = require('kanagawa.colors').setup()
-
       require('kanagawa').setup({
-        overrides = {
-          Normal = { bg = 'NONE' },
-          NvimTreeNormal = { bg = 'NONE' },
-          TelescopeBorder = { bg = 'NONE' },
-          tabline_a_normal = { bg = dc.waveAqua2, fg = dc.sumiInk0 },
-          tabline_b_normal = { bg = dc.sumiInk0, fg = dc.waveAqua2 },
-          tabline_c_normal = { bg = dc.sumiInk0, fg = dc.waveAqua2 },
-        },
+        overrides = function(colors)
+          return {
+            Normal = { bg = 'NONE' },
+            NvimTreeNormal = { bg = 'NONE' },
+            TelescopeBorder = { bg = 'NONE' },
+            LineNr = { bg = 'NONE' },
+            SignColumn = { bg = 'NONE' },
+            GitSignsAdd = { bg = 'NONE' },
+            GitSignsChange = { bg = 'NONE' },
+            GitSignsDelete = { bg = 'NONE' },
+            tabline_a_normal = { bg = colors.palette.waveAqua2, fg = colors.palette.sumiInk0 },
+            tabline_b_normal = { bg = colors.palette.sumiInk0, fg = colors.palette.waveAqua2 },
+            tabline_c_normal = { bg = colors.palette.sumiInk0, fg = colors.palette.waveAqua2 },
+          }
+        end,
       })
 
       vim.cmd('colorscheme kanagawa')
