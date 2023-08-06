dd80276e7525bb41a1b92c00cbd33175ab73c2d3
lbavendanos <lbavendanos@gmail.com>
2023-08-05 19:15:35 -0500

    Fix colorschema

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -1,30 +1,60 @@
+local groups = {
+  Normal = { bg = 'NONE' },
+  NormalNC = { bg = 'NONE' },
+  WinBar = { bg = 'NONE' },
+  WinBarNC = { bg = 'NONE' },
+  NvimTreeNormal = { bg = 'NONE' },
+  TelescopeBorder = { bg = 'NONE' },
+  LineNr = { bg = 'NONE' },
+  SignColumn = { bg = 'NONE' },
+  GitSignsAdd = { bg = 'NONE' },
+  GitSignsChange = { bg = 'NONE' },
+  GitSignsDelete = { bg = 'NONE' },
+}
+
 return {
-  -- { 'catppuccin/nvim', name = 'catppuccin' },
-  -- 'EdenEast/nightfox.nvim',
   {
     'rebelot/kanagawa.nvim',
+    enabled = false,
     lazy = false,
     priority = 1000,
     config = function()
       require('kanagawa').setup({
-        overrides = function(colors)
-          return {
-            Normal = { bg = 'NONE' },
-            NvimTreeNormal = { bg = 'NONE' },
-            TelescopeBorder = { bg = 'NONE' },
-            LineNr = { bg = 'NONE' },
-            SignColumn = { bg = 'NONE' },
-            GitSignsAdd = { bg = 'NONE' },
-            GitSignsChange = { bg = 'NONE' },
-            GitSignsDelete = { bg = 'NONE' },
-            tabline_a_normal = { bg = colors.palette.waveAqua2, fg = colors.palette.sumiInk0 },
-            tabline_b_normal = { bg = colors.palette.sumiInk0, fg = colors.palette.waveAqua2 },
-            tabline_c_normal = { bg = colors.palette.sumiInk0, fg = colors.palette.waveAqua2 },
-          }
+        overrides = function()
+          return groups
         end,
       })
 
       vim.cmd('colorscheme kanagawa')
+    end,
+  },
+  {
+    'EdenEast/nightfox.nvim',
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('nightfox').setup({
+        groups = {
+          all = groups,
+        },
+      })
+
+      vim.cmd('colorscheme duskfox')
+    end,
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    enabled = true,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup({
+        custom_highlights = groups,
+      })
+
+      vim.cmd('colorscheme catppuccin-mocha')
     end,
   },
 }
