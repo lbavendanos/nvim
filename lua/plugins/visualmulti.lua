3a00c3530ba5763116565a4e8b972b519fd62161
lbavendanos <lbavendanos@gmail.com>
2023-09-18 22:41:14 -0500

    Fix visualmulti

--- lua/plugins/visualmulti.lua
+++ lua/plugins/visualmulti.lua
@@ -2,4 +2,10 @@ return {
   'mg979/vim-visual-multi',
   branch = 'master',
   event = 'BufReadPost',
+  init = function()
+    vim.g.VM_maps = {
+      ['Add Cursor Down'] = '<M-C-Down>',
+      ['Select Cursor Up'] = '<M-C-Up>',
+    }
+  end,
 }
