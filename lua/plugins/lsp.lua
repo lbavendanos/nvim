b8733ab492b5eb73503d63863af49f47cd49eb8f
lbavendanos <lbavendanos@gmail.com>
2023-09-24 19:25:07 -0500

    Fix lua lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,7 +3,9 @@ return {
   dependencies = {
     'williamboman/mason.nvim',
     'jose-elias-alvarez/null-ls.nvim',
+    'hrsh7th/nvim-cmp',
     { 'folke/neoconf.nvim', cmd = 'Neoconf', config = true },
+    { 'folke/neodev.nvim', config = true },
     { 'ray-x/lsp_signature.nvim', config = { hi_parameter = 'IncSearch' } },
     'nvim-telescope/telescope.nvim',
     'nvimdev/lspsaga.nvim',
@@ -90,26 +92,42 @@ return {
     lspconfig.lua_ls.setup({
       on_attach = on_attach,
       capabilities = capabilities,
-      settings = {
-        Lua = {
-          runtime = {
-            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-            version = 'LuaJIT',
-          },
-          diagnostics = {
-            -- Get the language server to recognize the `vim` global
-            globals = { 'vim' },
-          },
-          workspace = {
-            -- Make the server aware of Neovim runtime files
-            library = vim.api.nvim_get_runtime_file('', true),
-          },
-          -- Do not send telemetry data containing a randomized but unique identifier
-          telemetry = {
-            enable = false,
-          },
-        },
-      },
+      on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+          client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+            Lua = {
+              runtime = {
+                -- Tell the language server which version of Lua you're using
+                -- (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+              },
+              diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' },
+              },
+              -- Make the server aware of Neovim runtime files
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  vim.env.VIMRUNTIME,
+                  -- "${3rd}/luv/library"
+                  -- "${3rd}/busted/library",
+                },
+                -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                -- library = vim.api.nvim_get_runtime_file("", true)
+              },
+              -- Do not send telemetry data containing a randomized but unique identifier
+              telemetry = {
+                enable = false,
+              },
+            },
+          })
+
+          client.notify('workspace/didChangeConfiguration', { settings = client.config.settings })
+        end
+        return true
+      end,
     })
 
     lspconfig.intelephense.setup({
